8582f4ecc56cad1a7440b39b0b4500aa38a7dd8f
Karol Wieczorek <karol.wieczorek@outlook.com>
2018-09-06 18:50:36 +0200

    Added NetworkingExample project

--- Networking.podspec
+++ Networking.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name            = "Networking"
-  s.version         = "0.0.2"
+  s.version         = "0.0.1"
   s.summary         = "HTTP networking library."
   s.description     = "HTTP networking library written in Swift."
   s.homepage        = "http://EXAMPLE/" #TODO: Add valid URL
---
+++ NetworkingExample/NetworkingExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NetworkingExample
+//
+//  Created by Karol on 06.09.2018.
+//  Copyright © 2018 Tooploox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
