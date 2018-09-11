acf6b9e52ab58d9d1bf79f740f0b854d8150c924
Pawel Chmiel <pawel.chmiel@tooploox.com>
2018-09-11 11:48:16 +0200

    Example project with tableview for displaying content

---
+++ NetworkingExample/NetworkingExample/Repository.swift
@@ -0,0 +1,14 @@
+//
+//  Repository.swift
+//  NetworkingExample
+//
+//  Created by Paweł Chmiel on 11.09.2018.
+//  Copyright © 2018 Tooploox. All rights reserved.
+//
+
+import Foundation
+
+struct Repository: Codable {
+    let id: Int
+    let name: String
+}
--- NetworkingExample/NetworkingExample/ViewController.swift
+++ NetworkingExample/NetworkingExample/ViewController.swift
@@ -9,34 +9,86 @@
 import UIKit
 import Networking
 
-struct Model: Codable {
-    let id: Int
-    let name: String
-}
+final class ViewController: UIViewController {
 
-class ViewController: UIViewController {
+    private var repositories: [Repository]? {
+        didSet {
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    private let tableView = UITableView()
+    
+    //Session configuration
+    private let sessionConfiguration = SessionConfiguration(host: "https://api.github.com")
+    private lazy var apiService = ApiService(configuration: sessionConfiguration)
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .purple
     
-        requestRepos(for: "pchmiel")
+        setupTableView()
+        
+        requestRepos(for: "google")
     }
     
-    private func requestRepos(for user: String) {
-        let sessionConfiguration = SessionConfiguration(host: "https://api.github.com")
-        let apiService = ApiService(configuration: sessionConfiguration)
-        let apiRequest = ApiRequest(endpoint: "/users/\(user)/repos", method: .GET)
+    private func requestRepos(for organization: String) {
         
-        apiService.send(request: apiRequest) { (result: Result<[Model], ApiError>) in
-            print(result)
+        let apiRequest = ApiRequest(endpoint: "/orgs/\(organization)/repos", method: .GET)
+        
+        apiService.send(request: apiRequest) { [weak self] (result: Result<[Repository], ApiError>) in
+            result.ifSuccess { repositories in
+                self?.repositories = repositories
+            }.else(closure: { error in
+                DispatchQueue.main.async {
+                    self?.showAlertWith(error)
+                }
+            })
         }
     }
-    
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 }
+ 
+extension ViewController {
+   private func setupTableView() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "repositoryCell")
+        tableView.delegate = self
+        tableView.dataSource = self
+    
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
+    }
+    
+    private func showAlertWith(_ error: ApiError) {
+        let alertViewController = UIAlertController(title: "Erorr", message: error.localizedDescription, preferredStyle: .alert)
+        let okButton = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        alertViewController.addAction(okButton)
+        
+        present(alertViewController, animated: true, completion: nil)
+    }
+ }
 
+ extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return repositories?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "repositoryCell"),
+            let repositoryName = repositories?[indexPath.row].name else {
+                return UITableViewCell()
+        }
+    
+        cell.textLabel?.text = repositoryName
+        
+        return cell
+    }
+ }
